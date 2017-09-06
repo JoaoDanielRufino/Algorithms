7a0aa82f7e86c08cdae7ff6808774bf3e62faa39
JoaoDanielRufino <joaodaniel0405@gmail.com>
2017-09-06 14:33:04 -0300

    Create insertion_sort.cpp

---
+++ Sort/insertion_sort.cpp
@@ -0,0 +1,43 @@
+/*Complexity: worst case: O(n^2).
+              best case: O(n).
+              */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void insertion_sort(vector<int> &vet){
+    int j,elem;
+    for(int i = 1; i < vet.size(); i++){
+        elem = vet[i];
+        j = i - 1;
+        while(j >= 0 && vet[j] > elem)
+            vet[j+1] = vet[j--];
+        vet[j+1] = elem;
+    }
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    int n,a;
+    vector<int> vet;
+
+    cout << "input size: ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cout << "Number[" << i+1 << "]: ";
+        cin >> a;
+        vet.push_back(a);
+    }
+
+    insertion_sort(vet);
+
+    cout << endl << "Numbers sorted: ";
+    for(int i = 0; i < n; i++)
+        cout << vet[i] << " ";
+
+    cout << endl;
+
+    return 0;
+}
