571e8eab678839fc5aebf6030e39d74da57c7a61
JoaoDanielRufino <joaodaniel0405@gmail.com>
2017-09-06 14:00:15 -0300

    Create bubble_sort.cpp

---
+++ Sort/bubble_sort.cpp
@@ -0,0 +1,49 @@
+/*Complexity: worst case: O(n^2).
+              best case: O(n).
+              */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubble_sort(vector<int> &vet){
+    bool flag;
+    for(int i = vet.size()-1; i > 0; i--){
+        flag = false;
+        for(int j = 0; j < i; j++){
+            if(vet[j] > vet[j+1]){
+                vet[j] = vet[j] ^ vet[j+1];
+                vet[j+1] = vet[j] ^ vet[j+1];
+                vet[j] = vet[j] ^ vet[j+1];
+                flag = true;
+            }
+        }
+        if(!flag)
+            break;
+    }
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    int n,a;
+    vector<int> vet;
+
+    cout << "input size: ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cout << "Number[" << i+1 << "]: ";
+        cin >> a;
+        vet.push_back(a);
+    }
+
+    bubble_sort(vet);
+
+    cout << endl << "Numbers sorted: ";
+    for(int i = 0; i < n; i++)
+        cout << vet[i] << " ";
+
+    cout << endl;
+
+    return 0;
+}
