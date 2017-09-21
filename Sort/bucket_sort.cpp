0e5bf2f3f2964aec73675adbad0d254cbdc07037
JoaoDanielRufino <joaodaniel0405@gmail.com>
2017-09-21 14:00:25 -0300

    Create bucket_sort.cpp

---
+++ Sort/bucket_sort.cpp
@@ -0,0 +1,48 @@
+/*Complexity: worst case: O(n).
+              best case: O(n).
+              */
+#include <bits/stdc++.h>
+#define N 100
+
+using namespace std;
+
+void bucket_sort(vector<int> &vet){
+    int aux[N] = {0};
+    for(int i = 0; i < vet.size(); i++)
+        aux[vet[i]]++;
+    int i = 0;
+    for(int j = 0; j < N; j++){
+        while(aux[j] > 0){
+            aux[j] -= 1;
+            vet[i++] = j;
+        }
+    }
+}
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    int n,a;
+    vector<int> vet;
+
+    cout << "input size: ";
+    cin >> n;
+
+    cout << "Random numbers:";
+    srand(time(NULL));
+    for(int i = 0; i < n; i++){
+        a = rand()%N;
+        vet.push_back(a);
+        cout << " " << a;
+    }
+
+    bucket_sort(vet);
+
+    cout << endl << "Numbers sorted: ";
+    for(int i = 0; i < n; i++)
+        cout << vet[i] << " ";
+
+    cout << endl;
+
+    return 0;
+}
