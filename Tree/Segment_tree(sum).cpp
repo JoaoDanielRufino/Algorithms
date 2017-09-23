f7fafecb8b97a2940cab5165d14da02f8e514781
JoaoDanielRufino <joaodaniel0405@gmail.com>
2017-09-22 23:45:08 -0300

    Update Segment_tree(sum).cpp

--- Tree/Segment_tree(sum).cpp
+++ Tree/Segment_tree(sum).cpp
@@ -3,30 +3,6 @@
 
 using namespace std;
 
-int make_st(int *vet, int left, int right, int *st, int index){
-    int mid = left + (right - left)/2;
-
-    if(left == right){
-        st[index] = vet[left];
-        return vet[left];
-    }
-
-    st[index] = make_st(vet, left, mid, st, index*2+1) +
-                make_st(vet, mid+1, right, st, index*2+2);
-
-    return st[index];
-}
-
-int *initiate_st(int *vet, int size){
-    int x = (int) (ceil(log2(size)));
-    int max_size = 2 * (int) pow(2, x) - 1;
-    int *st = new int[max_size];
-
-    make_st(vet, 0, size-1, st, 0);
-
-    return st;
-}
-
 void update_st(int *st, int left, int right, int i, int dif, int index){
     if(i < left || i > right)
         return;
@@ -59,6 +35,30 @@ int get_sum(int *st, int left, int right, int qs, int qe, int index){
 
 int sum(int *st, int lastpos, int qs, int qe){
     return get_sum(st, 0, lastpos, qs, qe, 0);
+}
+
+int make_st(int *vet, int left, int right, int *st, int index){
+    int mid = left + (right - left)/2;
+
+    if(left == right){
+        st[index] = vet[left];
+        return vet[left];
+    }
+
+    st[index] = make_st(vet, left, mid, st, index*2+1) +
+                make_st(vet, mid+1, right, st, index*2+2);
+
+    return st[index];
+}
+
+int *initiate_st(int *vet, int size){
+    int x = (int) (ceil(log2(size)));
+    int max_size = 2 * (int) pow(2, x) - 1;
+    int *st = new int[max_size];
+
+    make_st(vet, 0, size-1, st, 0);
+
+    return st;
 }
 
 int main(){
