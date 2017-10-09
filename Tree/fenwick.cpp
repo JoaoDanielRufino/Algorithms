9b0ca3c797801946f5b0ff5b81e693cf36db0960
João Daniel Rufino <joaodaniel0405@gmail.com>
2017-10-08 21:22:36 -0300

    Create fenwick.cpp

---
+++ Tree/fenwick.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define N 100
+
+using namespace std;
+
+int sum(int *tree, int index){
+	int sum = 0;
+	index++;
+
+	while(index > 0){
+		sum += tree[index];
+		index = index - (index & -index);
+	}
+	return sum;
+}
+
+void update(int *tree, int n, int index, int data){
+	index++;
+	while(index <= n){
+		tree[index] += data;
+		index = index + (index & -index);
+	}
+}
+
+int* construct(vector<int> vet, int n){
+	int *tree = new int[n+1];
+
+	for(int i = 1; i <= n; i++)
+		tree[i] = 0;
+
+	for(int i = 0; i < n; i++)
+		update(tree, n, i, vet[i]);
+
+	return tree;
+}
+
+int main(){
+
+	  ios::sync_with_stdio(false); //cin.tie(0);
+    int n,a,*tree,v;
+	  vector<int> vet;
+
+	  cout << "input size: ";
+    cin >> n;
+	  tree = new int[n+1];
+
+	  cout << "Random numbers: ";
+	  srand(time(NULL));
+	  for(int i = 0; i < n; i++){
+      a = rand()%N;
+      vet.push_back(a);
+      cout << " " << a;
+	  }
+
+	  tree = construct(vet, n);
+
+	  cout << endl << endl << "Index for the sum: ";
+    cin >> a;
+    cout << "Sum of values [0-" << a << "]" << " = " << sum(tree, a) << endl;
+
+    cout << "Update a value [index-value]: ";
+    cin >> a >> v;
+    update(tree, n, a, v);
+
+    cout << "New index for the sum: ";
+    cin >> a;
+    cout << "Sum of updated values [0-" << a << "]" << " = " << sum(tree, a) << endl;
+
+	  cout << endl;
+
+    return 0;
+}
