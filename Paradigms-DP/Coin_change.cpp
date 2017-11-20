6a30778072911da041fed94121e43302199cf5e1
João Daniel Rufino <joaodaniel0405@gmail.com>
2017-11-20 15:33:39 -0200

    Create Coin_change.cpp

---
+++ Paradigms-DP/Coin_change.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[10005][5]; //Max value+1 | qtd of coins.
+
+long long int coin_change(vector<int> vet, int value, int index){
+    if(!value)
+        return 1;
+
+    if(value < 0 || index == vet.size())
+        return 0;
+
+    if(dp[value][index] != -1)
+        return dp[value][index];
+
+    return dp[value][index] = coin_change(vet, value-vet[index], index) + coin_change(vet, value, index+1);
+}
+
+int main(){
+
+    ios::sync_with_stdio(false); cin.tie(0);
+    int val;
+    vector<int> vet;
+
+    vet.push_back(1); //Exemple of coins.
+    vet.push_back(5);
+    vet.push_back(10);
+    vet.push_back(25);
+    vet.push_back(50);
+    memset(dp, -1, sizeof dp);
+
+    while(cin >> val){
+        cout << coin_change(vet, val, 0) << endl;
+    }
+
+    return 0;
+}
