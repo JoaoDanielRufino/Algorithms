1f689d14937103fe1643a5f127c82d0942e64365
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-04-05 21:01:57 -0300

    Create linked_list_cycle.cpp

---
+++ Simple Data Structures/linked_list_cycle.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow, *fast;
+        
+        slow = fast = head;
+        while(slow && fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
+                return true;
+        }
+        
+        return false;
+    }
+};
