e50fecb6325bc170007580579eb914ac38bd8640
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-01-16 12:01:53 -0300

    Update merge_intervals.cpp

--- Array/merge_intervals.cpp
+++ Array/merge_intervals.cpp
@@ -2,32 +2,21 @@
 
 class Solution {
 public:
-    static bool myCompare(const vector<int> a, const vector<int> b) {
-        if(a[0] < b[0])
-            return true;
-        if(a[0] == b[0] && a[1] < b[1])
-            return true;
-        return false;
-    }
-    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(!intervals.size() || intervals.size() == 1)
-            return intervals;
-        
-        vector< vector<int> > res;
-        
-        sort(intervals.begin(), intervals.end(), myCompare);
-        
-        int lastElem = -1;
-        for(auto interval : intervals) {
-            if(lastElem == -1 || res[lastElem][1] < interval[0]) {
-                res.push_back(interval);
-                lastElem++;
+        vector<vector<int>> ans;
+
+        sort(intervals.begin(), intervals.end());
+
+        int last = -1;
+        for(vector<int> interval : intervals) {
+            if(last == -1 || ans[last][1] < interval[0]) {
+                ans.push_back(interval);
+                last++;
+            } else {
+                ans[last][1] = max(ans[last][1], interval[1]);
             }
-            else
-                res[lastElem][1] = max(res[lastElem][1], interval[1]);
         }
-        
-        return res;
+
+        return ans;
     }
 };
