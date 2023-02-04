0d05ddfb814cf40f14df7a44623d76b550ee940c
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-02-04 13:05:24 -0300

    Update find_all_anagrams_in_a_string.cpp

--- String/find_all_anagrams_in_a_string.cpp
+++ String/find_all_anagrams_in_a_string.cpp
@@ -34,6 +34,52 @@ public:
             hash[s[r]]--;
         }
         
+// Easier solution
+class Solution {
+public:
+    bool equal(vector<int>& a, vector<int>& b) {
+        for(int i = 0; i < a.size(); i++) {
+            if(a[i] != b[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+
+        if(p.size() > s.size())
+            return ans;
+
+        vector<int> mpS(26), mpP(26);
+
+        for(int i = 0; i < p.size(); i++) {
+            mpP[p[i] - 'a']++;
+            mpS[s[i] - 'a']++;
+        }
+
+        int l = 0;
+        int r = p.size() - 1;
+        while(r < s.size()) {
+            if(equal(mpS, mpP))
+                ans.push_back(l);
+
+            mpS[s[l] - 'a']--;
+
+            l++;
+            r++;
+
+            if(r >= s.size())
+                break;
+
+            mpS[s[r] - 'a']++;
+        }
+
+        return ans;
+    }
+};
+        
         return res;
     }
 };
