46a9fd74266a53eff45e430f4fea9ef4ce059c91
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-05-23 10:49:23 -0300

    Update the_number_of_beautiful_subsets.go

--- Backtracking/the_number_of_beautiful_subsets.go
+++ Backtracking/the_number_of_beautiful_subsets.go
@@ -21,3 +21,27 @@ func solve(nums []int, k int, subset map[int]int, i int) int {
 func beautifulSubsets(nums []int, k int) int {
     return solve(nums, k, map[int]int{}, 0)
 }
+
+// Wth for loop
+func solve(nums []int, k int, subset map[int]int, start int) int {
+    if start >= len(nums) {
+        return 0
+    }
+
+    ans := 0
+    for i := start; i < len(nums); i++ {
+        diff := nums[i] - k
+        sum := nums[i] + k
+        if subset[diff] == 0 && subset[sum] == 0 {
+            subset[nums[i]]++
+            ans += 1 + solve(nums, k, subset, i + 1)
+            subset[nums[i]]--
+        }
+    }
+
+    return ans
+}
+
+func beautifulSubsets(nums []int, k int) int {
+    return solve(nums, k, map[int]int{}, 0)
+}
