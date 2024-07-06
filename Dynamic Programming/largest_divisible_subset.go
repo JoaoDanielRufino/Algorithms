769e4295cd80fec86fe95eaa35a253e17ce1e432
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-07-06 11:48:07 -0300

    Create largest_divisible_subset.go

---
+++ Dynamic Programming/largest_divisible_subset.go
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/largest-divisible-subset/description/
+
+func canAdd(nums []int, x int) bool {
+    if len(nums) == 0 {
+        return true
+    }
+
+    lastPos := len(nums)-1
+
+    return nums[lastPos] % x == 0 || x % nums[lastPos] == 0
+}
+
+func solve(nums []int, i int, curr []int, subset *[]int, dp []int) {
+    if i >= len(nums) {
+        if len(curr) > len(*subset) {
+            tmp := make([]int, len(curr))
+            copy(tmp, curr)
+            *subset = tmp
+        }
+        return
+    }
+    
+    if canAdd(curr, nums[i]) && len(curr)+1 > dp[i] {
+        dp[i] = len(curr)+1
+        solve(nums, i + 1, append(curr, nums[i]), subset, dp)
+    }
+
+    solve(nums, i + 1, curr, subset, dp)
+}
+
+func largestDivisibleSubset(nums []int) []int {
+    subset := []int{}    
+    dp := make([]int, len(nums))
+
+    sort.Ints(nums)
+    
+    solve(nums, 0, []int{}, &subset, dp)
+    
+    return subset
+}
