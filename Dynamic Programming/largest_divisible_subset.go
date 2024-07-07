cf00cde4d0f42ac19e20377c2792101bf530d4ca
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-07-07 10:30:25 -0300

    Update largest_divisible_subset.go

--- Dynamic Programming/largest_divisible_subset.go
+++ Dynamic Programming/largest_divisible_subset.go
@@ -38,3 +38,40 @@ func largestDivisibleSubset(nums []int) []int {
     
     return subset
 }
+
+// or with for loop
+func canAdd(nums []int, x int) bool {
+    if len(nums) == 0 {
+        return true
+    }
+
+    lastPos := len(nums)-1
+
+    return nums[lastPos] % x == 0 || x % nums[lastPos] == 0
+}
+
+func solve(nums []int, start int, curr []int, subset *[]int, dp []int) {
+    if len(curr) > len(*subset) {
+        tmp := make([]int, len(curr))
+        copy(tmp, curr)
+        *subset = tmp
+    }
+    
+    for i := start; i < len(nums); i++ {
+        if canAdd(curr, nums[i]) && len(curr)+1 > dp[i] {
+            dp[i] = len(curr)+1
+            solve(nums, i + 1, append(curr, nums[i]), subset, dp)
+        }
+    }
+}
+
+func largestDivisibleSubset(nums []int) []int {
+    subset := []int{}    
+    dp := make([]int, len(nums))
+
+    sort.Ints(nums)
+    
+    solve(nums, 0, []int{}, &subset, dp)
+    
+    return subset
+}
